8fb94c93f1727b543e4125cbbfc939803e2f9bf0
SupremeA <supreme036@gmail.com>
2014-10-15 03:13:04 +0000

    Created edit and create post page and styled

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,32 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
+     def create
+     @post = Post.new(params.require(:post).permit(:title, :body))
+     if @post.save
+       flash[:notice] = "POW! That awesome post was saved."
+       redirect_to @post
+     else
+       flash[:error] = "There was an error saving the post. It was you not me, so Please try again."
+       render :new
+     end
+   end
+
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+     @post = Post.find(params[:id])
+     if @post.update_attributes(params.require(:post).permit(:title, :body))
+       flash[:notice] = "Ok! That post was good so I updated it for you."
+       redirect_to @post
+     else
+       flash[:error] = "There was an error saving the post. So keep tryin'."
+       render :edit
+     end
+   end
 end
