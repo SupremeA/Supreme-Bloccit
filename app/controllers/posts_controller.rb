3f52be3183a177c55ee39ea80e506aa123176ab5
SupremeA <supreme036@gmail.com>
2014-10-16 05:49:42 +0000

    Associated posts to users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
   end
 
      def create
-     @post = Post.new(params.require(:post).permit(:title, :body))
+     @post = current_user.posts.build(params.require(:post).permit(:title, :body))
      if @post.save
        flash[:notice] = "POW! That awesome post was saved."
        redirect_to @post
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
 require 'faker'
 
+5.times do
+  user = User.new(
+  name:      Faker::Name.name,
+  email:     Faker::Internet.email,
+  password:  Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
 50.times do
   Post.create!(
+  user:  users.sample,
   title: Faker::Lorem.sentence,
-  body: Faker::Lorem.paragraph
+  body:  Faker::Lorem.paragraph
   )
 end
 posts = Post.all
@@ -17,4 +29,14 @@ end
 
 puts "Seed finished"
 puts "#{Post.count} post created"
+puts "#{Comment.count} comments created"
+
+User.first.update_attributes!(
+email: 'supreme036@gmail.com',
+password: '12345678',
+)
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Post.count} posts cretaed"
 puts "#{Comment.count} comments created"
