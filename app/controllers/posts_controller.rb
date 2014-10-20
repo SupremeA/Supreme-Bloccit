f482d6772d1ff6e050e7ca75da67735039cf9a50
SupremeA <supreme036@gmail.com>
2014-10-20 04:21:17 +0000

    updated edit user interface

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
 
      def create
      @topic = Topic.find(params[:topic_id])
-     @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+     @post = current_user.posts.build(post_params)
      @post.topic = @topic
       authorize @post
 
@@ -37,12 +37,18 @@ class PostsController < ApplicationController
      @post = Post.find(params[:id])
      authorize @post
 
-     if @post.update_attributes(params.require(:post).permit(:title, :body))
+     if @post.update_attributes(post_params)
        flash[:notice] = "Ok! That post was good so I updated it for you."
        redirect_to [@topic, @post]
      else
        flash[:error] = "There was an error saving the post. So keep tryin'."
        render :new
      end
+   end
+
+   private
+
+   def post_params
+     params.require(:post).permit(:title, :body)
    end
 end
