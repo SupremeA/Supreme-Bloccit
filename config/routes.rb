df2591b5ea82a905c8b797ae442811b92468edcb
SupremeA <supreme036@gmail.com>
2014-11-12 21:17:41 +0000

    votes fix

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Bloccit::Application.routes.draw do
       resources :comments, only: [:create, :destroy]
         resources :favorites, only: [:create, :destroy]
 
-      post '/up_vote' => 'votes#up_vote', as: :up_vote
+      post '/up-vote' => 'votes#up_vote', as: :up_vote
       post '/down-vote' => 'votes#down_vote', as: :down_vote
     end
   end
--- db/migrate/20141101053404_create_votes.rb
+++ db/migrate/20141101053404_create_votes.rb
@@ -2,8 +2,8 @@ class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
       t.integer :value
-      t.string :user_id
-      t.string :post_id
+      t.references :user, index: true
+      t.references :post, index: true
 
       t.timestamps
     end
