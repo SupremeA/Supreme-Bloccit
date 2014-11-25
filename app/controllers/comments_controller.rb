008bde759886d5154bde50c5622aa83aae8b3600
SupremeA <supreme036@gmail.com>
2014-11-25 04:37:42 +0000

    added ajax delete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,31 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js
 
   def create
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.new(params_comment)
     @comment.user_id = current_user.id
     if @comment.save
-      redirect_to [@topic, @post], notice: "Your comment was saved! Awesome!"
+      redirect_to [@post.topic, @post], notice: "Your comment was saved! Awesome!"
     else
-      redirect_to [@topic, @post], notice: "Your comment didnt save"
+      redirect_to [@post.topic, @post], notice: "Your comment didnt save"
   end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
-
     authorize @comment
+
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
+
     else
       flash[:error] = "Comment was not deleted, please try again!"
-      redirect_to [@topic, @post]
+
+    end
+
+    respond_with(@comment) do |format|
+      format.html { redirect_to [@post.topic, @post] }
     end
   end
 end
